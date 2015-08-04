7d8f2b1c1fbf2a2b8ae323cb184f309bed45073f
Liu Chang <lllccc2009@gmail.com>
2015-08-03 23:50:21 -0400

    add wrong version of ex6

--- sed/exercise/ex6.sh
+++ sed/exercise/ex6.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
-for i in `ls /`
+for i in `ls /` # as it is just a string, so the loops never goes inside for every situation
 do
-	echo $i
+	if [ -L "$i" ]
+	then
+		echo "symbolic file"
+	elif [ -f "$i" ]
+	then
+		echo "plain file"
+	elif [ -d "$i" ]
+	then
+		echo "directory"
+	fi	
+	#echo $i
 done
